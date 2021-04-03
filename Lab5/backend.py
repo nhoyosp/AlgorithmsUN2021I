e4cf8213a595f4de810ceaedab086ab07dbdd9fe
Nicolas Hoyos <49196666+nhoyosp@users.noreply.github.com>
2021-04-03 15:36:14 -0500

    archivos python anadidos

---
+++ Lab5/backend.py
@@ -0,0 +1,11 @@
+def return__edad(edad):
+    return edad
+
+def decision(edad):
+    if(edad > 60):
+        return "usted esta en poblacion de riesgo"
+    else:
+        return "usted no esta en poblacion de riesgo"
+
+
+#creditos: http://pythonhow.com/data/unit_converter/backend.py
